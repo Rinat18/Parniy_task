9ede52bea266a8f489d3b87aa5d55f0f1e9da70e
Rinat Dosmatov <rinatdosmatov80@gmail.com>
2024-02-13 13:47:15 +0600

    asd

--- src/context/CartContextProvider.jsx
+++ src/context/CartContextProvider.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useReducer } from "react";
 import { ACTIONS } from "../helpers/consts";
+import { calcSubPrice, calcTotalPrice, getlocalStorage } from "../helpers/function";
 
 const cartContext = createContext();
 export const UseCart = () => useContext(cartContext);
@@ -17,11 +18,80 @@ const reducer = (state = INIT_STATE, action) => {
   }
 };
 const CartContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer , INIT_STATE);
+  const [state, dispatch] = useReducer(reducer, INIT_STATE);
 
+  // ! GET FROM CART
+
+  const getFromCart = () => {
+    let cart = getlocalStorage();
+    if (!cart) {
+      localStorage.setItem(
+        "cart",
+        JSON.stringify({
+          products: [],
+          totalPrice: 0,
+        })
+      );
+      cart = {
+        products: [],
+      };
+    }
+    dispatch({
+      type: ACTIONS.GET_CART,
+      payload: cart,
+    });
+  };
+
+  // ! ADD TO CART
+
+  const addToCart = (product) => {
+    let cart = getlocalStorage();
+    let newProduct = {
+      item: product,
+      count: 1,
+      subPrice: product.price,
+    };
+
+    let productToFind = cart.products.filter(
+      (elem) => elem.item.id === product.id
+    );
+    if (productToFind.length === 0) {
+      cart.products.push(newProduct);
+    } else {
+      cart.products = cart.products.filter(
+        (elem) => elem.item.id !== product.id
+      );
+    }
+    cart.totalPrice = calcTotalPrice(cart.products)
+    localStorage.setItem("cart", JSON.stringify(cart));
+    dispatch({
+      type: ACTIONS.GET_CART,
+      payload: cart,
+    });
+  };
+
+  const getCount = (id, count) => {
+    let cart = getlocalStorage();
+    cart.products.map((elem) => {
+      if (elem.item.id === id) {
+        elem.count = count;
+        elem.subPrice = calcSubPrice(elem);
+        cart.totalPrice = calcTotalPrice(cart.products);
+      }
+      return elem;
+    });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    dispatch({
+      type: ACTIONS.GET_CART,
+      payload: cart,
+    });
+  };
   const values = {
-
-  }
+    getFromCart,
+    cart: state.cart,
+    addToCart,
+    getCount,
+  };
   return <cartContext.Provider value={values}>{children}</cartContext.Provider>;
 };
 
--- src/routes/MainRoutesProvider.jsx
+++ src/routes/MainRoutesProvider.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import HomePage from '../pages/HomePage'
-import DetailCards from '../pages/DetailCards'
-import AddPage from '../pages/AddPage'
-import Payment from '../pages/Payment'
-import { Route, Routes } from 'react-router-dom'
-import Favorites from '../pages/Favorites'
-import EditModal from '../components/modal/EditModal'
+import React from "react";
+import HomePage from "../pages/HomePage";
+import DetailCards from "../pages/DetailCards";
+import AddPage from "../pages/AddPage";
+import Payment from "../pages/Payment";
+import { Route, Routes } from "react-router-dom";
+import Favorites from "../pages/Favorites";
+import EditModal from "../components/modal/EditModal";
+import Cart from "../components/cart/Cart";
 
 const MainRoutesProvider = () => {
   const PUBLIC_ROUTES = [
-    {id:1, link:"/", element:<HomePage />} ,
-    {id:2, link:"/detail/:id", element:<DetailCards />} ,
-    {id:3, link:"/add", element:<AddPage />} ,
-    {id:4, link:"/payment", element:<Payment />} ,
-    {id:5, link:"/favorites", element:<Favorites />} ,
-    {id:6, link:"/edit/:id", element:<EditModal />} ,
-  ]
+    { id: 1, link: "/", element: <HomePage /> },
+    { id: 2, link: "/detail/:id", element: <DetailCards /> },
+    { id: 3, link: "/add", element: <AddPage /> },
+    { id: 4, link: "/payment", element: <Payment /> },
+    { id: 5, link: "/favorites", element: <Favorites /> },
+    { id: 6, link: "/edit/:id", element: <EditModal /> },
+    { id: 7, link: "/cart", element: <Cart /> },
+  ];
   return (
     <>
       <Routes>
@@ -24,7 +26,7 @@ const MainRoutesProvider = () => {
         ))}
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default MainRoutesProvider
+export default MainRoutesProvider;
